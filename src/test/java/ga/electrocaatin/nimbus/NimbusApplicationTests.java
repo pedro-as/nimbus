32961e3692a6597e128e63674d9295fb31eaa6e9
Pedro Amorim Sá <amorimp@pm.me>
2022-12-12 22:51:45 -0300

    init project

---
+++ src/test/java/ga/electrocaatin/nimbus/NimbusApplicationTests.java
@@ -0,0 +1,13 @@
+package ga.electrocaatin.nimbus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NimbusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
